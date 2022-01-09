ce2f51be55206b47cde270d4c6a4f4cf3504c5e4
ganeshjadhav221b <ganeshjadhav221b@gmail.com>
2022-01-09 15:18:42 +0530

    first_negative_subarray

--- array/basic.py
+++ array/basic.py
@@ -21,5 +21,53 @@ def max_sum_subarray(arr, n, k =3):
 		# print(i,j,subArrayTotal,maximum)
 	return maximum
 arr = [3,5,-1,6,7,-10,9]
-print(max_sum_subarray(arr, len(arr),3))
+# print(max_sum_subarray(arr, len(arr),3))
 
+
+#sliding window problem
+#find first negative in subarray of size k
+
+#i/p->[3,-5,1,6,-7,-10,9] , k = 3
+#O/p -> -5,-5, -7, -7,-7
+
+#i/p->[3,-5,-1,6,-7,-10,9] , k = 3
+#O/p -> -5,-5, -1,-7,-7
+
+
+#i/p -> [3,5,7,-1,10]
+#o/p ->None, -1, -1
+
+#i/p -> [-3,5,7,1,10]
+#o/p -> -3, None,None
+
+def first_negative_subarray(arr, n, k=3):
+	i = 0
+	j = 0
+	negativeList = []
+	resultList = []
+	while j < n:
+		# print(i,j,subArrayTotal)
+
+		element = arr[j]
+		if element < 0:
+			negativeList.append(element)
+		if (j - i + 1) < k:		#1. If window size isnt reached, keep incrementing j
+			j += 1
+		else:
+			#2. perform calculation and store in variable 
+			res = negativeList[0] if len(negativeList)>0 else None
+			resultList.append(res)
+			# print(i,j,element,res,resultList)
+			#3. discard earlier calculation
+			if res == arr[i]:
+				negativeList.pop(0)
+			#4. shift window
+			i += 1 
+			j += 1
+		# print('Here:',element, resultList, negativeList)
+	return resultList
+arr = [3,-5,1,6,-7,-10,9]
+arr = [3,-5,-1,6,-7,-10,9]
+arr = [3,5,7,-1,10]
+arr = [-3,5,7,1,10]
+print(first_negative_subarray(arr, len(arr),3))
